37e3e75e8efb3874d5608b21765d2519abf4d87f
Nazar Vladyka <nazarvladyka00@gmail.com>
2019-02-23 17:51:59 +0200

    create class which will encrypt-decrypt messages

---
+++ src/main/java/com/nazarvladyka/Client.java
@@ -0,0 +1,4 @@
+package com.nazarvladyka;
+
+public class Client {
+}
---
+++ src/main/java/com/nazarvladyka/Encrypt.java
@@ -0,0 +1,52 @@
+package com.nazarvladyka;
+
+import java.util.Random;
+
+public class Encrypt {
+    private int userId;
+    private int key;
+
+    public Encrypt(int userId) {
+        Random random = new Random();
+        int minKey = 100000;
+        this.userId = userId;
+        key = random.nextInt() + minKey;
+    }
+
+    public static String xorMessage(String message, String key) {
+        try {
+            if (message == null || key == null) return null;
+
+            char[] cKey = key.toCharArray();
+            char[] cMessage = message.toCharArray();
+
+            int messageLength = cMessage.length;
+            int keyLength = cKey.length;
+            char[] newMessage = new char[messageLength];
+
+            for (int i = 0; i < messageLength; i++) {
+                newMessage[i] = (char)(cMessage[i] ^ cKey[i % keyLength]);
+            }
+
+            return new String(newMessage);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    //    public static void main(String[] args) {
+//        String txt = "716253";
+//        String key1 = "082738";
+//        String key2 = "526890";
+//
+//        System.out.println(txt);
+//        String txt1 = xorMessage(txt, key1);
+//        System.out.println(txt1);
+//        String txt12 = xorMessage(txt1, key2);
+//        System.out.println(txt12);
+//        String txt2 = xorMessage(txt12, key1);
+//        System.out.println(txt2);
+//        String txt0 = xorMessage(txt2, key2);
+//        System.out.println(txt0);
+//    }
+}
