ac05526f7cd8522e0d0fda336e88d43c799e5d03
Nazar Vladyka <nazarvladyka00@gmail.com>
2019-03-06 12:53:45 +0200

    split code on the server to few methods

--- src/main/java/com/nazarvladyka/Server.java
+++ src/main/java/com/nazarvladyka/Server.java
@@ -50,51 +50,13 @@ public class Server {
                     acceptSocketChanel.register(selector, SelectionKey.OP_READ);
                 } else if (selectionKey.isReadable()) {
                     SocketChannel readSocketChannel = (SocketChannel) selectionKey.channel();
-                    ByteBuffer byteBuffer = ByteBuffer.allocate(50);
-
-                    readSocketChannel.read(byteBuffer);
-
-                    String outString = new String(byteBuffer.array());
-                    request = new ArrayList<>(Arrays.asList(outString.split(" : ")));
-                    requests.add(request);
-
-                    senderId = getKeyByValue(clients, readSocketChannel.getRemoteAddress().toString());
-                    receiverId = request.get(2);
-                    message = request.get(3);
-                    phase = request.get(1);
-                    messageId = request.get(0);
-
-                    request.add(senderId);
-
-                    log("INCOMING: " + "USER" + senderId + " sent to USER" + receiverId + " MESSAGE : " + "\'" + message + "\'" +
-                            "| phase = " + phase + "| messageId = " + messageId);
-
-                    byteBuffer.clear();
+                    read(readSocketChannel);
 
                     readSocketChannel.register(selector, SelectionKey.OP_WRITE);
                 } else if (selectionKey.isWritable()) {
                     SocketChannel writeSocketChannel = (SocketChannel) selectionKey.channel();
-                    ByteBuffer byteBuffer = ByteBuffer.allocate(50);
+                    write(writeSocketChannel);
 
-                    for (ArrayList request : requests) {
-                        receiverAddress = clients.get(request.get(2));
-                        if(writeSocketChannel.getRemoteAddress().toString().equals(receiverAddress)) {
-                            senderId = request.get(4).toString();
-                            receiverId = request.get(2).toString();
-                            message = request.get(3).toString();
-                            phase = request.get(1).toString();
-                            messageId = request.get(0).toString();
-
-                            byte[] bytes = message.getBytes();
-                            writeSocketChannel.write(ByteBuffer.wrap(bytes));
-
-                            log("OUTGOING: " + "USER" + senderId + " sent to USER" + receiverId + " MESSAGE : " + "\'" + message + "\'" +
-                                    "| phase = " + phase + "| messageId = " + messageId);
-                            requests.remove(request);
-                        }
-                    }
-                    byteBuffer.clear();
-                    byteBuffer.flip();
                     writeSocketChannel.register(selector, SelectionKey.OP_READ);
                 }
                 selectionKeyIterator.remove();
@@ -113,5 +75,51 @@ public class Server {
             }
         }
         return "";
+    }
+
+    private static void write(SocketChannel writeSocketChannel) throws IOException {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(50);
+
+        for (ArrayList request : requests) {
+            receiverAddress = clients.get(request.get(2));
+            if(writeSocketChannel.getRemoteAddress().toString().equals(receiverAddress)) {
+                senderId = request.get(4).toString();
+                receiverId = request.get(2).toString();
+                message = request.get(3).toString();
+                phase = request.get(1).toString();
+                messageId = request.get(0).toString();
+
+                byte[] bytes = message.getBytes();
+                writeSocketChannel.write(ByteBuffer.wrap(bytes));
+
+                log("OUTGOING: " + "USER" + senderId + " sent to USER" + receiverId + " MESSAGE : " + "\'" + message + "\'" +
+                        "| phase = " + phase + "| messageId = " + messageId);
+                requests.remove(request);
+            }
+        }
+        byteBuffer.clear();
+        byteBuffer.flip();
+    }
+
+    private static void read(SocketChannel readSocketChannel) throws IOException {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(50);
+        readSocketChannel.read(byteBuffer);
+
+        String outString = new String(byteBuffer.array());
+        request = new ArrayList<>(Arrays.asList(outString.split(" : ")));
+        requests.add(request);
+
+        senderId = getKeyByValue(clients, readSocketChannel.getRemoteAddress().toString());
+        receiverId = request.get(2);
+        message = request.get(3);
+        phase = request.get(1);
+        messageId = request.get(0);
+
+        request.add(senderId);
+
+        log("INCOMING: " + "USER" + senderId + " sent to USER" + receiverId + " MESSAGE : " + "\'" + message + "\'" +
+                "| phase = " + phase + "| messageId = " + messageId);
+
+        byteBuffer.clear();
     }
 }
